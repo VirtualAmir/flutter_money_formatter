80dfc200ac1c1f66b37ad9bf74d9198242e23f19
Amir Thapa <amirthapa@Amirs-MacBook-Pro.local>
2023-10-02 13:51:54 +0545

    upgrade: to dart 3

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/wisdomekeh/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wisdomekeh/workspace/flutter/flutter_money_formatter/example"
+export "FLUTTER_ROOT=/Users/amirthapa/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/amirthapa/Documents/Projects/flutter_money_formatter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
